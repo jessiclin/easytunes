a66ed55acaa786f7109ef27cb4e73061603d6794
Maxwell Testa <maxwelltesta@MacBook-Pro-3.local>
2020-10-28 14:29:45 -0400

    a

--- src/components/home_screen/HomeScreen.js
+++ src/components/home_screen/HomeScreen.js
@@ -1,19 +1,13 @@
 import React, { useState, Component } from 'react'
-<<<<<<< HEAD:src/components/pages/HomeScreen/HomeScreen.js
-import { Button } from '../../Button/Button';
-import '../../Navbar/Navbar.css';
-import Dropdown from '../../Dropdown';
-=======
 import { Button } from '../Button/Button';
 import { NavLink, Link } from 'react-router-dom';
 import '../Navbar/Navbar.css';
 import Dropdown from '../Dropdown';
 import { BsMusicNoteList } from "react-icons/bs";
->>>>>>> a594391d99022ff73b04b7e517658cc8da24892d:src/components/home_screen/HomeScreen.js
 import {AiFillHome,AiFillHeart, AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
 import "./HomeScreen.css"
 import Logo from "./am4a.png"
-import AccountButton from '../../AccountButton/AccountButton'
+import AccountButton from '../AccountButton/AccountButton'
 class HomeScreen extends Component {
 
     
