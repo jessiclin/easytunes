7c149a0781e63fbeb6fe21e4d017d2d1b619b862
Maxwell Testa <maxwelltesta@MacBook-Pro-3.local>
2020-10-27 20:35:35 -0400

    Search

---
+++ src/components/pages/Search/SearchList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import SearchListCard from './SearchListCard.js'
+
+import './Songlist.css'
+class SearchList extends React.Component {
+    state = { 
+        
+    }
+    
+    render() {
+        const searchList = this.props.searchList;
+        const items = searchList.items;
+
+        return (
+            <div>
+                {items && items.map(function(item) {
+                    return (
+                        <SearchListCard searchList={searchList} item={item}/>
+                    );})
+                }
+            </div>
+        );
+    }
+}
+const mapStateToProps = (state, ownProps) => {
+    const searchList = ownProps.searchList;
+    return {
+
+    };
+}
+export default SearchList;
