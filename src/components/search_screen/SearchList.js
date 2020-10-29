acbd62c1c57bbb76b210e392b3209a88bb2882a6
Maxwell Testa <maxwelltesta@MacBook-Pro-3.local>
2020-10-29 10:49:38 -0400

    search

--- src/components/search_screen/SearchList.js
+++ src/components/search_screen/SearchList.js
@@ -7,8 +7,13 @@ class SearchList extends React.Component {
     }
     
     render() {
-        const searchList = this.props.searchList;
+        const searchList = {"items": [
+            {"song_title":"21 Guns", "artist":"Green Day", "song_length":"2:34"},
+            {"song_title":"Money Machine", "artist":"100 Gecs", "song_length":"9:59"},
+            {"song_title":"Static Buzz", "artist":"Snail Mail", "song_length":"1:56"}
+        ]};
         const items = searchList.items;
+        console.log(items)
 
         return (
             <div className="container gray">
@@ -19,8 +24,9 @@ class SearchList extends React.Component {
                     <div class='col s3'>x</div>
                 </div>
                 {items && items.map(function(item) {
+                    console.log(item)
                     return (
-                        <SearchListCard searchList={searchList} item={item}/>
+                        <SearchListCard item={item}/>
                     );})
                 }
             </div>
