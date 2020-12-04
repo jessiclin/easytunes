8352bdc225a1be33076df4f85a65f96365363979
jessiclin <linje1202@gmail.com>
2020-12-04 13:51:27 -0500

    Popup appears for name when forking

--- frontend/src/components/Profile/Playlists/NewPlaylistButton.js
+++ frontend/src/components/Profile/Playlists/NewPlaylistButton.js
@@ -17,7 +17,8 @@ class NewPlaylistButton extends Component {
         popupVisible: false,
         playlistName: null,
         username : this.props.username,
-        user_id : this.props.user_id
+        user_id : this.props.user_id,
+        error: ""
      }
 
     // Set new playlist box to visible 
@@ -37,7 +38,7 @@ class NewPlaylistButton extends Component {
 
     // Create a new playlist 
     createNewPlaylist = () =>{
-        this.setInvisible()
+        
         let requestBody = {
             query: `
                 mutation {
@@ -63,43 +64,49 @@ class NewPlaylistButton extends Component {
             })
             .then(result => {
                  // Update the playlists on the UI 
-                requestBody = {
-                    query: `
-                        query {
-                            getUserPlaylists(username: "${this.state.username}"){
-                                _id
-                                name
-                                username
-                                likes
-                                total_duration 
-                                public
-                                songs {
-                                    song_id
+
+                if (result.errors)
+                    this.setState({error: result.errors[0].message})
+                else {
+                    requestBody = {
+                        query: `
+                            query {
+                                getUserPlaylists(username: "${this.state.username}"){
+                                    _id
                                     name
+                                    username
+                                    likes
+                                    total_duration 
+                                    public
+                                    songs {
+                                        song_id
+                                        name
+                                    }
                                 }
                             }
-                        }
-                    `
-                }
+                        `
+                    }
 
-                fetch("http://localhost:5000/graphql", {
-                    method: 'POST',
-                    body: JSON.stringify(requestBody),
-                    headers: {
-                    'content-type': 'application/json'
-                    }})
-                .then(res => {
-                    if (res.status !== 200 && res.status !== 201) 
-                        throw new Error('Failed');
-                    return res.json()
-                })
-                .then(result => {
-                    console.log(result)
-                    this.setPlaylists(result.data.getUserPlaylists)
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+                    fetch("http://localhost:5000/graphql", {
+                        method: 'POST',
+                        body: JSON.stringify(requestBody),
+                        headers: {
+                        'content-type': 'application/json'
+                        }})
+                    .then(res => {
+                        if (res.status !== 200 && res.status !== 201) 
+                            throw new Error('Failed');
+                        return res.json()
+                    })
+                    .then(result => {
+                        console.log(result)
+                        this.setInvisible()
+                        this.setPlaylists(result.data.getUserPlaylists)
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
             })
             .catch(err => {
                 console.log(err);
@@ -114,6 +121,7 @@ class NewPlaylistButton extends Component {
                     {this.state.popupVisible ? 
                     <div className="new-playlist-box">
                        Playlist Name
+                       <div className = "error-box"> {this.state.error} </div>
                         <input type="text" required onChange={this.inputOnChange}/>
                         <button className = "confirm-new-btn" onClick={this.createNewPlaylist}> <AiOutlineCheckCircle size = {24}/></button>
                         <button className = "cancel-new-btn"  onClick={this.setInvisible}> <AiOutlineCloseCircle size = {24}/></button>
